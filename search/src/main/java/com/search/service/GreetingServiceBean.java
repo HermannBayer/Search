e88ea1161f8f44c9cfc350b8fc29c78524f2a6cc
HermannBayer <hermann.bayer@aon.at>
2021-06-10 15:05:51 +0200

    c5

--- search/src/main/java/com/search/service/GreetingServiceBean.java
+++ search/src/main/java/com/search/service/GreetingServiceBean.java
@@ -45,6 +45,6 @@ public class GreetingServiceBean implements GreetingService {
 
     @Override
     public void delete(String id) {
-        repository.deleteById(id);
+        repository.delete(id);
     }
 }
