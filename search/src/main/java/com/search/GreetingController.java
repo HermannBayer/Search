0caaab3f1e4318f656df16d5d0350c7408c0da26
HermannBayer <hermann.bayer@aon.at>
2021-06-11 10:46:07 +0200

    bereinigt

--- search/src/main/java/com/search/GreetingController.java
+++ search/src/main/java/com/search/GreetingController.java
@@ -1,7 +1,7 @@
 package com.search;
 
 
-import com.search.service.GreetingService;
+//import com.search.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,43 +34,4 @@ public class GreetingController {
     }
 
     
-/*
-    @ResponseBody
-    @RequestMapping(value = "/greetings", method = RequestMethod.GET)
-    public ResponseEntity<List<Greeting>> getAll() {
-        return new ResponseEntity<List<Greeting>>(greetingService.getAll(), HttpStatus.OK);
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "/greetings", method = RequestMethod.POST)
-    public ResponseEntity<Greeting> insertGreeting(@RequestBody Greeting greeting) {
-        return new ResponseEntity<Greeting>(greetingService.create(greeting), HttpStatus.CREATED);
-    	
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "/greetings", method = RequestMethod.PUT)
-    public ResponseEntity<Greeting> updateGreeting(@RequestBody Greeting greeting) {
-        return new ResponseEntity<Greeting>(greetingService.update(greeting), HttpStatus.MOVED_PERMANENTLY);
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "/greetings/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<Greeting> deleteGreeting(@PathVariable("id") String idd) {
-        greetingService.delete(idd);
-        return new ResponseEntity<Greeting>(HttpStatus.NO_CONTENT);
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "/greetings{id}", method = RequestMethod.POST)
-    public ResponseEntity<Greeting> getOne(@PathVariable("id") String idd) {
-        return new ResponseEntity<Greeting>(greetingService.findOne(idd), HttpStatus.OK);
-    }
-
-    @ResponseBody
-    @RequestMapping(value = "/greetings/{name}", method = RequestMethod.GET)
-    public ResponseEntity<List<Greeting>> getByUserName(@PathVariable("name") String name) {
-        return new ResponseEntity<List<Greeting>>(greetingService.getGreetingByUsername(name), HttpStatus.OK);
-    }
-*/
 }
--- search/src/main/java/com/search/config/ElasticsearchConfig.java
+++
@@ -1,5 +0,0 @@
-package com.search.config;
-
-public class ElasticsearchConfig {
-
-}
